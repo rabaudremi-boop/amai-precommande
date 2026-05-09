24a8dd5b3874c2bde809a6f0bca94c89634de20c
Rémi Rabaud <rabaudremi@gmail.com>
2026-05-09 19:21:33 +0200

    chore(firebase): rotate api key after leak alert

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,7 +5,7 @@ import { getFirestore, type Firestore } from 'firebase/firestore';
 // not a secret. Security comes from Firestore rules, not API key obscurity.
 // Project: amai-precommande (https://console.firebase.google.com/project/amai-precommande)
 const firebaseConfig = {
-  apiKey: 'AIzaSyBxu_z6S5NnaHZy1iJR5F1VO2Un9R3ka0A',
+  apiKey: 'AIzaSyC6x1BrKkQXN1Ozxx4NjedozooOT3X8cUY',
   authDomain: 'amai-precommande.firebaseapp.com',
   projectId: 'amai-precommande',
   storageBucket: 'amai-precommande.firebasestorage.app',
